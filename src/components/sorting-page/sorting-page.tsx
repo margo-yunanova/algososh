56239532b248bbf3f1bc063c6f6433baf717c945
Margo <m.yunanova@gmail.com>
2023-05-04 23:00:31 +0300

    implement visualization for bubble sort

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -8,46 +8,80 @@ import { Column } from "../ui/column/column";
 import { Container } from "../content/container";
 import { FC } from "react";
 import { delay, randomArr, swap } from "../../constants/constants";
+import { ElementStates } from "../../types/element-states";
+import { DELAY_IN_MS } from "../../constants/delays";
 
 const realConsoleLog = console.log;
 
 export const SortingPage: FC = () => {
-  const [checkedRadioButton, setCheckedRadioButton] = useState("Выбор");
+  type TSortingAlgorithm = 'selectionSort' | 'bubbleSort';
+
+  const [checkedRadioButton, setCheckedRadioButton] = useState<TSortingAlgorithm>('selectionSort');
   const [isLoader, setLoader] = useState<boolean>();
   const [sortedArray, setSortedArray] = useState(() => randomArr());
-  const [currentIndex, setCurrentIndex] = useState<Number>();
+  const [activeButton, setActiveButton] = useState<String | null>(null);
+  const [currentIndex, setCurrentIndex] = useState<number | undefined>(
+    undefined
+  );
+  const [sortedColumnIndex, setSortedColumnIndex] = useState<number | undefined>(undefined);
+
+
+
 
   const bubbleSort = async (array: Array<number>, direction: Direction) => {
     if (direction === Direction.Ascending) {
       for (let i = array.length - 1; i > 0; i--) {
         for (let j = 0; j < i; j++) {
-          await delay(1000);
+          setCurrentIndex(j);
+          await delay(DELAY_IN_MS);
           if (array[j] > array[j + 1]) {
             swap(array, j);
             setSortedArray([...array]);
           }
         }
+        setSortedColumnIndex(i);
       }
     } else {
       for (let i = array.length - 1; i > 0; i--) {
         for (let j = 0; j < i; j++) {
-          await delay(1000);
+          setCurrentIndex(j);
+          await delay(DELAY_IN_MS);
           if (array[j] < array[j + 1]) {
             swap(array, j);
             setSortedArray([...array]);
           }
         }
+        setSortedColumnIndex(i);
       }
     }
-
-    setLoader(false);
+    setCurrentIndex(undefined);
+    setSortedColumnIndex(0);
+    setActiveButton(null);
   };
 
   const sort = (direction: Direction) => {
-    if (checkedRadioButton === "Пузырёк") {
+    if (checkedRadioButton === 'bubbleSort') {
       bubbleSort(sortedArray, direction);
     } else {
     }
+  };
+
+  const setRadioInputHandler = (buttonName: TSortingAlgorithm) => {
+    setActiveButton(null);
+    setCheckedRadioButton(buttonName);
+  };
+
+  const setDirectionHandler = (direction: Direction) => {
+    setSortedColumnIndex(undefined);
+    setActiveButton(direction);
+    sort(direction);
+  };
+
+  const getColumnState = (index: number): ElementStates | undefined => {
+    if (index === currentIndex || index + 1 === currentIndex)
+      return ElementStates.Changing;
+    if (sortedColumnIndex === undefined) return ElementStates.Default;
+    if (index >= sortedColumnIndex) return ElementStates.Modified;
   };
 
   return (
@@ -59,20 +93,14 @@ export const SortingPage: FC = () => {
               label="Выбор"
               name="sortOption"
               value="Выбор"
-              onClick={() => {
-                setLoader(false);
-                setCheckedRadioButton("Выбор");
-              }}
+              onClick={() => setRadioInputHandler('selectionSort')}
               defaultChecked
             />
             <RadioInput
               label="Пузырёк"
               name="sortOption"
               value="Пузырёк"
-              onClick={() => {
-                setLoader(false);
-                setCheckedRadioButton("Пузырёк");
-              }}
+              onClick={() => setRadioInputHandler('bubbleSort')}
             />
           </div>
           <div className={`${styles.menuItem} ${styles.menuSort}`}>
@@ -80,27 +108,33 @@ export const SortingPage: FC = () => {
               text="По возрастанию"
               sorting={Direction.Ascending}
               type="button"
-              isLoader={isLoader}
-              onClick={() => {
-                setLoader(true);
-                sort(Direction.Ascending);
-              }}
+              isLoader={activeButton === Direction.Ascending}
+              onClick={() => setDirectionHandler(Direction.Ascending)}
             />
             <Button
               text="По убыванию"
               sorting={Direction.Descending}
               type="button"
-              onClick={() => {
-                setLoader(true);
-                sort(Direction.Descending);
-              }}
+              isLoader={activeButton === Direction.Descending}
+              disabled={
+                activeButton === null
+                  ? false
+                  : activeButton !== Direction.Descending
+              } //TODO
+              onClick={() => setDirectionHandler(Direction.Descending)}
             />
           </div>
-          <Button text="Новый массив" />
+          <Button
+            text="Новый массив"
+            onClick={() => {
+              setSortedColumnIndex(undefined);
+              setSortedArray(randomArr());
+            }}
+          />
         </nav>
         <div className={styles.array}>
           {sortedArray.map((number, index) => (
-            <Column index={number} key={index} />
+            <Column state={getColumnState(index)} index={number} key={index} />
           ))}
         </div>
       </Container>
