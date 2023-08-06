7d327fb0c9672c7ad1a3cb797aabbc42c32dbc02
Margo <m.yunanova@gmail.com>
2023-08-06 20:57:11 +0300

    string component state refactoring

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -11,20 +11,28 @@ import { DELAY_IN_MS } from "../../constants/delays";
 
 export const StringComponent: React.FC = () => {
   const [inputValue, setInputValue] = useState<string>("");
-  const [dataForVisualization, setDataForVisualization] =
-    useState<Array<string> | null>(null);
-  const [firstIndex, setFirstIndex] = useState<number | undefined>(undefined);
-  const [secondIndex, setSecondIndex] = useState<number | undefined>(undefined);
+  const [{ firstIndex, secondIndex, data }, setDataForVisualization] =
+    useState<{
+      firstIndex?: number;
+      secondIndex?: number;
+      data: string[];
+    }>({
+      firstIndex: undefined,
+      secondIndex: undefined,
+      data: [],
+    });
   const [hasStartedReverseInput, setStartReverseInput] = useState(false);
 
   const reverseInput = async (input: string) => {
     const array = input.split("");
     let lastIndex = array.length - 1;
     for (let i = 0; i < array.length / 2; i++) {
-      setFirstIndex(i);
-      setSecondIndex(lastIndex);
       swap(array, i, lastIndex);
-      setDataForVisualization([...array]);
+      setDataForVisualization({
+        firstIndex: i,
+        secondIndex: lastIndex,
+        data: array,
+      });
       await delay(DELAY_IN_MS);
       lastIndex--;
     }
@@ -51,9 +59,11 @@ export const StringComponent: React.FC = () => {
 
   const submitHandler: FormEventHandler = async (e) => {
     e.preventDefault();
-    setFirstIndex(undefined);
-    setSecondIndex(undefined);
-    setDataForVisualization((inputValue as string).split(""));
+    setDataForVisualization({
+      firstIndex: undefined,
+      secondIndex: undefined,
+      data: inputValue.split(""),
+    });
     setStartReverseInput(true);
     await delay(DELAY_IN_MS);
     await reverseInput(inputValue);
@@ -81,14 +91,13 @@ export const StringComponent: React.FC = () => {
           />
         </form>
         <div className={styles.circle}>
-          {dataForVisualization &&
-            dataForVisualization.map((letter, index) => (
-              <Circle
-                letter={letter}
-                key={index}
-                state={getCircleState(index, firstIndex, secondIndex)}
-              />
-            ))}
+          {data.map((letter, index) => (
+            <Circle
+              letter={letter}
+              key={index}
+              state={getCircleState(index, firstIndex, secondIndex)}
+            />
+          ))}
         </div>
       </Container>
     </SolutionLayout>
