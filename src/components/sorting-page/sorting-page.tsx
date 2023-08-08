c4d8cc1bae425a8ac310763852a52f4fadacc830
Margo <m.yunanova@gmail.com>
2023-08-08 13:29:44 +0300

    refactor Sorting component

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -19,74 +19,95 @@ import { DELAY_IN_MS } from "../../constants/delays";
 export const SortingPage: FC = () => {
   type TSortingAlgorithm = "selectionSort" | "bubbleSort";
 
+  type TSortState = {
+    firstIndex?: number;
+    secondIndex?: number;
+    sortedArray: number[];
+    sortedColumnIndex?: number;
+  };
+
   const [checkedRadioButton, setCheckedRadioButton] =
     useState<TSortingAlgorithm>("selectionSort");
-  const [sortedArray, setSortedArray] = useState(() => randomArr(3, 17, 100));
   const [activeButton, setActiveButton] = useState<string | null>(null);
-  const [firstIndex, setFirstIndex] = useState<number | undefined>(undefined);
-  const [secondIndex, setSecondIndex] = useState<number | undefined>(undefined);
-  const [sortedColumnIndex, setSortedColumnIndex] = useState<
-    number | undefined
-  >(undefined);
+  const [{ firstIndex, secondIndex, sortedArray, sortedColumnIndex }, setData] =
+    useState<TSortState>(() => ({
+      firstIndex: undefined,
+      secondIndex: undefined,
+      sortedArray: randomArr(3, 17, 100),
+      sortedColumnIndex: undefined,
+    }));
 
   const bubbleSort = async (array: Array<number>, direction: Direction) => {
     for (let i = array.length - 1; i > 0; i--) {
       for (let j = 0; j < i; j++) {
-        setFirstIndex(j);
-        setSecondIndex(j + 1);
+        setData((state) => ({ ...state, firstIndex: j, secondIndex: j + 1 }));
         await delay(DELAY_IN_MS);
         if (compareArrayItems(direction, array, j, j + 1)) {
           swap(array, j, j + 1);
-          setSortedArray([...array]);
+          setData((state) => ({ ...state, sortedArray: [...array] }));
         }
       }
-      setSortedColumnIndex(i);
+      setData((state) => ({ ...state, sortedColumnIndex: i }));
     }
-    setFirstIndex(undefined);
-    setSecondIndex(undefined);
-    setSortedColumnIndex(0);
-    setActiveButton(null);
+    setData((state) => ({
+      ...state,
+      firstIndex: undefined,
+      secondIndex: undefined,
+      sortedColumnIndex: 0,
+    }));
   };
 
   const selectionSort = async (array: Array<number>, direction: Direction) => {
+    const state: TSortState = {
+      firstIndex: undefined,
+      secondIndex: undefined,
+      sortedArray: array,
+      sortedColumnIndex: undefined,
+    };
     for (let i = 0; i < array.length - 1; i++) {
-      setFirstIndex(i);
+      state.firstIndex = i;
+      setData({ ...state });
       let minIndex = i;
       for (let j = i + 1; j < array.length; j++) {
         await delay(DELAY_IN_MS);
-        setSecondIndex(j);
+        state.secondIndex = j;
+        setData({ ...state });
         if (compareArrayItems(direction, array, minIndex, j)) {
           minIndex = j;
         }
       }
       swap(array, i, minIndex);
-      setSortedArray([...array]);
-      setSortedColumnIndex(i);
+      state.sortedArray = [...array];
+      state.sortedColumnIndex = i;
+      setData({ ...state });
     }
-    setFirstIndex(undefined);
-    setSecondIndex(undefined);
-    setSortedColumnIndex(array.length - 1);
-    setActiveButton(null);
+    setData((state) => ({
+      ...state,
+      firstIndex: undefined,
+      secondIndex: undefined,
+      sortedColumnIndex: sortedArray.length - 1,
+    }));
   };
 
-  const sort = (direction: Direction) => {
+  const sort = async (direction: Direction) => {
     if (checkedRadioButton === "bubbleSort") {
-      bubbleSort(sortedArray, direction);
+      await bubbleSort(sortedArray, direction);
     } else {
-      selectionSort(sortedArray, direction);
+      await selectionSort(sortedArray, direction);
     }
   };
 
   const setRadioInputHandler = (buttonName: TSortingAlgorithm) => {
     setActiveButton(null);
-    setSortedColumnIndex(undefined);
+    setData((state) => ({ ...state, sortedColumnIndex: undefined }));
     setCheckedRadioButton(buttonName);
   };
 
-  const setDirectionHandler = (direction: Direction) => {
-    setSortedColumnIndex(undefined);
+  const setDirectionHandler = async (direction: Direction) => {
+    setData((state) => ({ ...state, sortedColumnIndex: undefined }));
     setActiveButton(direction);
-    sort(direction);
+    await sort(direction);
+    setActiveButton(null);
   };
 
   const getColumnState = (index: number): ElementStates | undefined => {
@@ -143,8 +164,11 @@ export const SortingPage: FC = () => {
             text="Новый массив"
             disabled={activeButton !== null}
             onClick={() => {
-              setSortedColumnIndex(undefined);
-              setSortedArray(randomArr(3, 17, 100));
+              setData((state) => ({
+                ...state,
+                sortedArray: randomArr(3, 17, 100),
+                sortedColumnIndex: undefined,
+              }));
             }}
           />
         </nav>
