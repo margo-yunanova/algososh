5a713a0eb725bfd3cf60dc28611a6fa1e693cea0
Margo <m.yunanova@gmail.com>
2023-08-09 12:53:41 +0300

    update string page e2e tests; #9

--- cypress/e2e/srting_page.cy.ts
+++ cypress/e2e/srting_page.cy.ts
@@ -1,14 +1,8 @@
 describe("The String Page", () => {
-  let colorChanged, colorDefault, colorModified;
-
   beforeEach(() => {
     cy.visit("/");
     cy.get('[data-cy="linkString"]').click();
     cy.url().should("include", "/recursion");
-
-    colorDefault = "rgb(0, 50, 255)";
-    colorModified = "rgb(127, 224, 81)";
-    colorChanged = "rgb(210, 82, 225)";
   });
 
   it("button is disabled", () => {
@@ -21,92 +15,98 @@ describe("The String Page", () => {
   });
 
   it("string is reversed", () => {
+    const colors = {
+      d: "rgb(0, 50, 255)",
+      m: "rgb(127, 224, 81)",
+      c: "rgb(210, 82, 225)",
+    };
     const words = ["привет", "тривеп", "теиврп", "тевирп"];
+    const colorsCod = ["dddddd", "cddddc", "mcddcm", "mmmmmm"];
 
     cy.get('[data-cy="input"]').should("be.empty").type(words[0]);
     cy.get('[data-cy="button"]').click();
 
-    for (const word of words) {
-      for (let i = 0; i < word.length; i++) {
-        cy.get('[data-cy="letter"]').eq(i).should("have.text", word[i]);
-      }
-    }
-  });
-
-  it("color", () => {
-    cy.get('[data-cy="input"]').should("be.empty").type("привет");
-    cy.get('[data-cy="button"]').click();
-
-    for (let i = 0; i < "привет".length; i++) {
-      cy.get('[data-cy="state"]')
-        .eq(i)
-        .should("have.css", "border-color")
-        .should("equal", colorDefault);
-    }
-
-    cy.get('[data-cy="state"]')
-      .eq(0)
-      .should("have.css", "border-color")
-      .should("equal", colorChanged);
-
-    for (let i = 1; i < 5; i++) {
-      cy.get('[data-cy="state"]')
-        .eq(i)
-        .should("have.css", "border-color")
-        .should("equal", colorDefault);
-    }
-
-    cy.get('[data-cy="state"]')
-      .eq(5)
-      .should("have.css", "border-color")
-      .should("equal", colorChanged);
-
-    cy.get('[data-cy="state"]')
-      .eq(0)
-      .should("have.css", "border-color")
-      .should("equal", colorModified);
-
-    cy.get('[data-cy="state"]')
-      .eq(1)
-      .should("have.css", "border-color")
-      .should("equal", colorChanged);
-
-    for (let i = 2; i < 4; i++) {
-      cy.get('[data-cy="state"]')
-        .eq(i)
-        .should("have.css", "border-color")
-        .should("equal", colorDefault);
-    }
-
-    cy.get('[data-cy="state"]')
-      .eq(4)
-      .should("have.css", "border-color")
-      .should("equal", colorChanged);
-
-    cy.get('[data-cy="state"]')
-      .eq(5)
-      .should("have.css", "border-color")
-      .should("equal", colorModified);
-
-    for (let i = 0; i < 6; i++) {
-      if (i === 2 || i === 3) {
+    for (let i = 0; i < words.length; i++) {
+      for (let j = 0; j < words[i].length; j++) {
+        cy.get('[data-cy="letter"]').eq(j).should("have.text", words[i][j]);
         cy.get('[data-cy="state"]')
-          .eq(i)
+          .eq(j)
           .should("have.css", "border-color")
-          .should("equal", colorChanged);
-      } else {
-        cy.get('[data-cy="state"]')
-          .eq(i)
-          .should("have.css", "border-color")
-          .should("equal", colorModified);
+          .should("equal", colors[colorsCod[i][j]]);
       }
     }
 
-    for (let i = 0; i < "привет".length; i++) {
-      cy.get('[data-cy="state"]')
-        .eq(i)
-        .should("have.css", "border-color")
-        .should("equal", colorModified);
-    }
+    // it("color", () => {
+    // const colorDefault = "rgb(0, 50, 255)";
+    // const colorModified = "rgb(127, 224, 81)";
+    // const colorChanged = "rgb(210, 82, 225)";
+    //   cy.get('[data-cy="input"]').should("be.empty").type("привет");
+    //   cy.get('[data-cy="button"]').click();
+
+    //   for (let i = 0; i < "привет".length; i++) {
+    //     cy.get('[data-cy="state"]')
+    //       .eq(i)
+    //       .should("have.css", "border-color")
+    //       .should("equal", colorDefault);
+    //   }
+
+    //   cy.get('[data-cy="state"]')
+    //     .eq(0)
+    //     .should("have.css", "border-color")
+    //     .should("equal", colorChanged);
+
+    //   for (let i = 1; i < 5; i++) {
+    //     cy.get('[data-cy="state"]')
+    //       .eq(i)
+    //       .should("have.css", "border-color")
+    //       .should("equal", colorDefault);
+    //   }
+
+    //   cy.get('[data-cy="state"]')
+    //     .eq(5)
+    //     .should("have.css", "border-color")
+    //     .should("equal", colorChanged);
+
+    //   cy.get('[data-cy="state"]')
+    //     .eq(0)
+    //     .should("have.css", "border-color")
+    //     .should("equal", colorModified);
+
+    //   cy.get('[data-cy="state"]')
+    //     .eq(1)
+    //     .should("have.css", "border-color")
+    //     .should("equal", colorChanged);
+
+    //   for (let i = 2; i < 4; i++) {
+    //     cy.get('[data-cy="state"]')
+    //       .eq(i)
+    //       .should("have.css", "border-color")
+    //       .should("equal", colorDefault);
+    //   }
+
+    //   cy.get('[data-cy="state"]')
+    //     .eq(4)
+    //     .should("have.css", "border-color")
+    //     .should("equal", colorChanged);
+
+    //   cy.get('[data-cy="state"]')
+    //     .eq(5)
+    //     .should("have.css", "border-color")
+    //     .should("equal", colorModified);
+
+    //   for (let i = 0; i < 6; i++) {
+    //     const color = i === 2 || i === 3 ? colorChanged : colorModified;
+    //     cy.get('[data-cy="state"]')
+    //       .eq(i)
+    //       .should("have.css", "border-color")
+    //       .should("equal", color);
+    //   }
+
+    //   for (let i = 0; i < "привет".length; i++) {
+    //     cy.get('[data-cy="state"]')
+    //       .eq(i)
+    //       .should("have.css", "border-color")
+    //       .should("equal", colorModified);
+    //   }
   });
 });
